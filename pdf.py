ec5048e2471b21b2923f2ff6674b749535b0f865
Pyzyryab <68871459+Pyzyryab@users.noreply.github.com>
2020-09-20 20:13:10 +0200

    First functional version

--- pdf.py
+++ pdf.py
@@ -1,13 +1,25 @@
 import PyPDF2
 import sys
+import optparse
 
+	
 
-operation = sys.argv[1]
+def take_command():
+
+	parser = optparse.OptionParser()
+	parser.add_option('-m', '--merge', dest='merge', help='Merge the desired PDF files into only one.')
+	parser.add_option('-w', '--watermark', dest='watermark', help='Put a watermark into your PDF')
+	
+	options, arguments = parser.parse_args()
+
+	return options
+	
 
 merger_inputs = sys.argv[2:]
-def pdf_combiner(pdf_list):
+def pdf_combiner(pdf_list, command):
 	'''Given PDF's files merge them into only one'''
 	pdf_name = str(input('How will your merged PDF will be named?\n') + '.pdf')
+	
 	#Creating a merger object
 	merger = PyPDF2.PdfFileMerger()
 
@@ -18,7 +30,7 @@ def pdf_combiner(pdf_list):
 	merger.write(pdf_name)
 
 
-def watermarker(pdf_list):
+def watermarker(pdf_list, command):
 
 	pdf_name = str(input('How will your watermarked PDF will be named?\n') + '.pdf')
 
@@ -34,17 +46,6 @@ def watermarker(pdf_list):
 		with open(pdf_name, 'wb') as file:
 			output.write(file)
 
-
-#Choose
-
-if operation == "-m":
-
-	pdf_combiner(merger_inputs)
-
-elif operation == "-w":
-
-	watermarker(merger_inputs)
-
-else:
-
-	print('Por favor, usa -m si quieres unir PDF\'s o -w si quieres poner a un PDF una marca de agua.')
+options = take_command()
+pdf_combiner(merger_inputs, options.merge)
+watermarker(merger_inputs, options.merge)
