107e5d1a005e07f0f8c1e7c087f56fa2cebf4d53
Pyzyryab <68871459+Pyzyryab@users.noreply.github.com>
2020-09-27 11:11:14 +0200

    'V1.0'

--- pdf.py
+++ pdf.py
@@ -1,36 +1,37 @@
 import PyPDF2
 import sys
-import optparse
+import argparse
 
-	
 
 def take_command():
 
-	parser = optparse.OptionParser()
-	parser.add_option('-m', '--merge', dest='merge', help='Merge the desired PDF files into only one.')
-	parser.add_option('-w', '--watermark', dest='watermark', help='Put a watermark into your PDF')
+	parser = argparse.ArgumentParser(description='This is a PDF toolkit suite')
 	
-	options, arguments = parser.parse_args()
-
+	parser.add_argument('-m', '--merge', dest='merge', nargs='+', help='Merge the desired PDF files into only one.')
+	parser.add_argument('-w', '--watermarker', dest='watermarker', nargs= '+', help='Put a watermark into your PDF')
+	
+	options = parser.parse_args()
+	
 	return options
 	
 
-merger_inputs = sys.argv[2:]
-def pdf_combiner(pdf_list, command):
+def pdf_combiner(pdf_list):
 	'''Given PDF's files merge them into only one'''
 	pdf_name = str(input('How will your merged PDF will be named?\n') + '.pdf')
 	
 	#Creating a merger object
 	merger = PyPDF2.PdfFileMerger()
+	print('Appeding your files. Wait...')
 
 	for pdf in pdf_list:
-		print('Appeding your files. Wait...')
+		
+		
 		merger.append(pdf)
 
 	merger.write(pdf_name)
 
 
-def watermarker(pdf_list, command):
+def pdf_watermarker(pdf_list):
 
 	pdf_name = str(input('How will your watermarked PDF will be named?\n') + '.pdf')
 
@@ -46,6 +47,22 @@ def watermarker(pdf_list, command):
 		with open(pdf_name, 'wb') as file:
 			output.write(file)
 
-options = take_command()
-pdf_combiner(merger_inputs, options.merge)
-watermarker(merger_inputs, options.merge)
+
+if __name__ == '__main__':
+
+	options = take_command()
+
+	merger_inputs = sys.argv[2:]
+
+	if options.merge:
+		
+		pdf_combiner(merger_inputs)
+		print(merger_inputs)
+
+	elif options.watermarker:
+
+		pdf_watermarker(merger_inputs)
+
+	else:
+
+		print('If you desire, use -h or --help to see how to use')
