36ecbcaacee1a14cc1ace02f51b69da1364a2cb9
Pyzyryab <68871459+Pyzyryab@users.noreply.github.com>
2020-10-11 22:08:49 +0200

    Added the split pdf pages funcionality

--- pdf.py
+++ pdf.py
@@ -5,22 +5,23 @@ import argparse
 
 def take_command():
 
-	parser = argparse.ArgumentParser(description='This is a PDF toolkit suite')
+	parser = argparse.ArgumentParser(description='''This is a PDF toolkit suite. 
+		Remember to write the relative/absolute path in everyone pdf file you insert or work directly 
+		in the same folder of the main script.''')
 	
 	parser.add_argument('-m', '--merge', dest='merge', nargs='+', help='Merge the desired PDF files into only one.')
 	parser.add_argument('-w', '--watermarker', dest='watermarker', nargs= '+', help='Put a watermark into your PDF')
-	
+	parser.add_argument('-s', '-splitter', dest='splitter', help='Split PDF pages and save it as a new file')
+
 	options = parser.parse_args()
 	
 	return options
 	
 
 def pdf_combiner(pdf_list):
-	'''Given PDF's files merge them into only one'''
 	
 	pdf_name = str(input('How will your merged PDF will be named?\n') + '.pdf')
 	
-	#Creating a merger object
 	merger = PyPDF2.PdfFileMerger()
 	print('Appeding your files. Wait...')
 
@@ -29,6 +30,31 @@ def pdf_combiner(pdf_list):
 		merger.append(pdf)
 
 	merger.write(pdf_name)
+
+
+def pdf_splitter(pdf_list):
+
+	original = PyPDF2.PdfFileReader(open(pdf_list[0], 'rb'))
+	original_pdf_name = str(original.getDocumentInfo().title)
+
+	for page in range(original.getNumPages()):
+		
+		output = PyPDF2.PdfFileWriter()
+
+		selected_page = original.getPage(page)
+
+		output.addPage(selected_page)
+
+		new_splitted_page_name = original_pdf_name + '_p' + str(page + 1) + '.pdf'
+
+		with open(new_splitted_page_name, 'wb') as file:
+			
+			output.write(file)
+
+		print(f'File {new_splitted_page_name} has been created succesfully.')
+
+	print('Congratulatios! Your files has been saved in your default path')
+
 
 
 def pdf_watermarker(pdf_list):
@@ -40,11 +66,15 @@ def pdf_watermarker(pdf_list):
 	output = PyPDF2.PdfFileWriter()
 
 	for i in range(original.getNumPages()):
+		
 		page = original.getPage(i)
+		
 		page.mergePage(watermark.getPage(0))
+		
 		output.addPage(page)
 
 		with open(pdf_name, 'wb') as file:
+			
 			output.write(file)
 
 
@@ -62,6 +92,10 @@ if __name__ == '__main__':
 	elif options.watermarker:
 
 		pdf_watermarker(merger_inputs)
+
+	elif options.splitter:
+
+		pdf_splitter(merger_inputs)
 
 	else:
 
